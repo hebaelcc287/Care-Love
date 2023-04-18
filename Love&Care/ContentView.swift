a6efdfbd94fd2df13a07fc1744fb2f58adc30879
hebaabdullah1 <127639276+hebaabdullah1@users.noreply.github.com>
2023-04-18 05:40:07 +0200

    Initial Commit

---
+++ Love&Care/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Love&Care
+//
+//  Created by Shimaa Elcc on 18.4.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Love&Care/Love_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_CareApp.swift
+//  Love&Care
+//
+//  Created by Shimaa Elcc on 18.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct Love_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
